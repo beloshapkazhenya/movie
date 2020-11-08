7cb336e664514e2360d4049d013eb110243e6d88
beloshapkazhenya <beloshapkazhenya@yandex.ru>
2020-11-08 13:47:57 +0300

    Updates

---
+++ precache-manifest.7e6f492cf07f5f25edc5b38c70499750.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd63ba3f26ca6d1b35c3a7a77dba0c4",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "6a2f75df9982e6da678c",
+    "url": "/movie/static/css/main.b7a2d957.chunk.css"
+  },
+  {
+    "revision": "8f7876b79f5f44e98411",
+    "url": "/movie/static/js/2.1aea2ba9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie/static/js/2.1aea2ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2f75df9982e6da678c",
+    "url": "/movie/static/js/main.33c3b6fb.chunk.js"
+  },
+  {
+    "revision": "342c4198987aead74a12",
+    "url": "/movie/static/js/runtime-main.bc8c7ce7.js"
+  },
+  {
+    "revision": "96522145e1af083105989b0d030af2bf",
+    "url": "/movie/static/media/Logo.96522145.svg"
+  },
+  {
+    "revision": "5d140e15b69ca15692b6b130e836f1a7",
+    "url": "/movie/static/media/cross.5d140e15.svg"
+  }
+]);
